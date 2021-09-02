d685a7bf73690c1c0325d1ba52157938a4c2ee54
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-09-02 14:48:25 -0300

    Criar classe SimulacaoService

---
+++ src/main/java/br/com/zup/ZupInvest/simulacao/SimulacaoService.java
@@ -0,0 +1,13 @@
+package br.com.zup.ZupInvest.simulacao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimulacaoService {
+    @Autowired
+    private SimulacaoRepository simulacaoRepository;
+
+
+
+}
