5cc660a74d429b24964f46227a6b5375084e88b1
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-09-02 14:25:12 -0300

    Criar classe SimulacaoRepository

---
+++ src/main/java/br/com/zup/ZupInvest/simulacao/SimulacaoRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.ZupInvest.simulacao;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SimulacaoRepository extends CrudRepository<Simulacao, String> {
+}
