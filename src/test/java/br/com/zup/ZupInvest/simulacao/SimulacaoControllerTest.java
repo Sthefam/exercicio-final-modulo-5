ef2d2b848faae9038b6b761fc957211fd5d18e33
Sthefam Ferreira Santos <mr.sthefam@gmail.com>
2021-09-03 18:39:12 -0300

    Criar classe SimulacaoControllerTest

---
+++ src/test/java/br/com/zup/ZupInvest/simulacao/SimulacaoControllerTest.java
@@ -0,0 +1,38 @@
+package br.com.zup.ZupInvest.simulacao;
+
+import br.com.zup.ZupInvest.simulacao.dtos.CadastroSimulacaoDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+
+import org.json.JSONString;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(SimulacaoController.class)
+public class SimulacaoControllerTest {
+    @MockBean
+    private SimulacaoService simulacaoService;
+
+    @SpyBean
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private MockMvc mockMVC;
+
+    private CadastroSimulacaoDTO simulacaoDTO;
+    private ObjectMapper objMapper;
+
+
+
+}
